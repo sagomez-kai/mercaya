40b3143cda6c9880e14c421e773ac3ae6e4c6187
HIHB <125474035+PachaLord@users.noreply.github.com>
2023-04-19 17:14:22 -0500

    Añadir función de calcular precio
    Se añadió una función que sirve para consultar la base de datos y devolver el resultado de la tienda que tiene la mayor cantidad de productos escritos en la lista, a la vez que esta tienda sería la más barata en donde comprar.

--- procesar.py
+++ procesar.py
@@ -71,3 +71,39 @@ def continuarLista(entrada, lista): #Añadir más productos a la lista. Esto es 
                 else:
                     i += 1
             lista.append(Producto(menor)) #Hacer un arreglo de objetos con cada producto
+            
+            # Define la lista de productos en los supermercados
+
+def calcular():
+  # Obtiene la lista de productos ingresada por el usuario
+  lista = input("Ingrese la lista de productos separados por comas: ").lower().split(",")
+
+  # Calcula el número de productos que se encuentran en cada supermercado
+  num_productos = []
+  for s in lista_productos:
+    num = 0
+    for p in lista:
+      if p in s:
+        num += 1
+    num_productos.append({"supermercado": s["supermercado"], "num": num})
+
+  # Ordena los supermercados por número de productos encontrados (de mayor a menor)
+  supermercados_por_productos = sorted(num_productos, key=lambda x: x["num"], reverse=True)
+
+  # Encuentra el supermercado más barato para la lista de productos
+  precio_minimo = float("inf")
+  supermercado_barato = ""
+  for s in lista_productos:
+    precio_total = 0
+    for p in lista:
+      if p in s and lista.index(p) != -1:
+        precio_total += s[p]
+    if precio_total < precio_minimo:
+      precio_minimo = precio_total
+      supermercado_barato = s["supermercado"]
+
+  # Muestra los resultados al usuario
+  resultado = f"El supermercado con más productos es {supermercados_por_productos[0]['supermercado']}.<br>" + \
+              f"El supermercado más barato para su lista de productos es {supermercado_barato}, con un precio total de {precio_minimo} pesos."
+  print(resultado)
+            
